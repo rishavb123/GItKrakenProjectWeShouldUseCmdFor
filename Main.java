63d1758d1a542b5707920107a9ed55370220c87d
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-05-31 10:37:22 -0400

    Combined all work into one single file

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        PerfectSquareInterfacez interfac = (int num) -> Math.sqrt(num) == Math.round(Math.sqrt(num));
+        System.out.println(interfac.PerfectSquareInterface(7));
+        PrintQuotient hi = (float a, float b) -> System.out.printf("%.3f / %.3f = %.3f", a, b, a / b);
+        hi.printQuotient(1, 3);
     }
 }
